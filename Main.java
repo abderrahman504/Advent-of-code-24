063bec7c458dd36560a258e1c6ec91fbfde2dcd8
abderrahman504 <rahmankhsherb@gmail.com>
2024-12-10 10:02:35 +0200

    Time measurement in main()

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main{
 	// Used to run the problem code for each day.
 	
 	public static void main(String[] args) {
-		Day4.part2();
+		var start = System.nanoTime();
+		Day10.solve(false);
+		var finish = System.nanoTime();
+		System.out.printf("Time : %.4f ms", (finish-start) * 1e-6);
 	}
 }
